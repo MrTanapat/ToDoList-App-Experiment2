e2801bef7d19aa5dab66d242ac56a661be65fa5a
SE_Tanapat <tan.tanapat2002@gmail.com>
2025-08-18 21:36:32 +0700

    Update build output to docs

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Experiment2/",
+  build: {
+    outDir: "docs", // เพิ่มบรรทัดนี้
+  },
 });
