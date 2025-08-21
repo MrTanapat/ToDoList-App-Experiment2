e4560b0008d317f8419c74706b00624d37206cf5
MrTanapat <tan.tanapat2002@gmail.com>
2025-08-21 21:49:41 +0700

    Install Commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Experiment2/",
-  build: {
-    outDir: "docs", // เพิ่มบรรทัดนี้
-  },
 });
