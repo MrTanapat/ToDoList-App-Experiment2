9ff922278d50f687d50f7c1db220e7be9fae006e
MrTanapat <tan.tanapat2002@gmail.com>
2025-08-21 22:14:57 +0700

    Fix: Corrected base path and homepage for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Experiment2/",
+  base: "/ToDoList-App-Experiment2/",
 });
